b274ad7c87457b5ad5a2d2816fb8ad8fe3acbadf
camila quimbaya <camila111paco@gmail.com>
2025-07-13 17:09:09 -0500

    feat:implemented animation on grid

--- src/notes/components/ListaNotas.tsx
+++ src/notes/components/ListaNotas.tsx
@@ -1,5 +1,6 @@
 import NotaCard from './NotaCard';
-import '../../styles/notaList.css'
+import { motion, AnimatePresence } from 'framer-motion';
+import '../../styles/notaList.css';
 
 interface Nota {
   _id: string;
@@ -13,27 +14,36 @@ interface Props {
   notas: Nota[];
   onActualizar: () => void;
   onEditar: (nota: Nota) => void;
-  vista: 'lista' | 'grid'; // 👈 nuevo
+  vista: 'lista' | 'grid';
 }
 
 const ListaNotas = ({ notas, onActualizar, onEditar, vista }: Props) => {
   return (
-    <div
+    <motion.div
+      layout
       className={`backgroundList p-6 rounded-lg shadow-lg grid gap-4 ${
         vista === 'grid' ? 'grid-cols-2' : 'grid-cols-1'
       }`}
     >
-
-  {notas.map(n => (
-    <NotaCard
-      key={n._id}
-      nota={n}
-      onNotaActualizada={onActualizar}
-      onEditar={onEditar}
-    />
-  ))}
-</div>
-
+      <AnimatePresence mode="popLayout">
+        {notas.map((n) => (
+          <motion.div
+            key={n._id}
+            layout
+            initial={{ opacity: 0, scale: 0.95 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.9 }}
+            transition={{ duration: 0.5 }}
+          >
+            <NotaCard
+              nota={n}
+              onNotaActualizada={onActualizar}
+              onEditar={onEditar}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
+    </motion.div>
   );
 };
 
