2d51278a869bef5e97895b582561dac91d19747c
camila quimbaya <camila111paco@gmail.com>
2025-07-13 01:13:17 -0500

    feat:implemented framer on routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,31 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 import Login from './auth/pages/Login';
 import PrivateRoute from './routes/PrivateRoute';
 import Dashboard from './notes/pages/Dashboard';
 import Register from './auth/pages/Register';
+import PageWrapper from './components/PageWrapper';
 import './index.css';
 
 const App = () => {
+  const location = useLocation();
+
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-
-      <Route
-        path="/"
-        element={
-          <PrivateRoute>
-            <Dashboard />
-          </PrivateRoute>
-        }
-      />
-
-      {/* Ruta fallback */}
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/login" element={<PageWrapper><Login /></PageWrapper>} />
+        <Route path="/register" element={<PageWrapper><Register /></PageWrapper>} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <PageWrapper><Dashboard /></PageWrapper>
+            </PrivateRoute>
+          }
+        />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </AnimatePresence>
   );
 };
 
